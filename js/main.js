e91946586ba45eb78baa8be4100fc05c89cc9f35
simioyin222 <simi.oyinkolade@gmail.com>
2024-03-25 18:46:35 -0500

    update code and add some base code to franework

--- js/main.js
+++ js/main.js
@@ -33,3 +33,22 @@ $('.open-overlay').click(function() {
       nav_item_5.removeClass('slide-in-nav-item-delay-4').addClass('slide-in-nav-item-delay-4-reverse');
     }
   })
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const navItems = document.querySelectorAll('.overlay-navigation ul li a');
+
+    navItems.forEach(item => {
+        item.addEventListener('click', function(e) {
+            e.preventDefault();
+            const targetId = this.getAttribute('href').substring(1);
+            const targetSection = document.getElementById(targetId);
+
+            if (targetSection) {
+                window.scrollTo({
+                    top: targetSection.offsetTop,
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
+});
