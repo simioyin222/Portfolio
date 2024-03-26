ff7f4a8c1286de61d1c54414d27e87a6e02d494f
simioyin222 <simi.oyinkolade@gmail.com>
2024-03-25 20:48:21 -0500

    change format of javascript and incorprated usage of velocity into project

--- js/main.js
+++ js/main.js
@@ -1,43 +1,47 @@
 $(document).ready(function() {
   console.log("jQuery is loaded");
-});
 
+  // Your existing code here...
+  // Remove or comment out the old '.open-overlay' click handler to avoid conflicts
 
-$('.open-overlay').click(function() {
-    var overlay_navigation = $('.overlay-navigation'),
-      nav_item_1 = $('nav li:nth-of-type(1)'),
-      nav_item_2 = $('nav li:nth-of-type(2)'),
-      nav_item_3 = $('nav li:nth-of-type(3)'),
-      nav_item_4 = $('nav li:nth-of-type(4)'),
-      nav_item_5 = $('nav li:nth-of-type(5)'),
-      top_bar = $('.bar-top'),
-      middle_bar = $('.bar-middle'),
-      bottom_bar = $('.bar-bottom');
-  
-    overlay_navigation.toggleClass('overlay-active');
-    if (overlay_navigation.hasClass('overlay-active')) {
-  
-      top_bar.removeClass('animate-out-top-bar').addClass('animate-top-bar');
-      middle_bar.removeClass('animate-out-middle-bar').addClass('animate-middle-bar');
-      bottom_bar.removeClass('animate-out-bottom-bar').addClass('animate-bottom-bar');
-      overlay_navigation.removeClass('overlay-slide-up').addClass('overlay-slide-down')
-      nav_item_1.removeClass('slide-in-nav-item-reverse').addClass('slide-in-nav-item');
-      nav_item_2.removeClass('slide-in-nav-item-delay-1-reverse').addClass('slide-in-nav-item-delay-1');
-      nav_item_3.removeClass('slide-in-nav-item-delay-2-reverse').addClass('slide-in-nav-item-delay-2');
-      nav_item_4.removeClass('slide-in-nav-item-delay-3-reverse').addClass('slide-in-nav-item-delay-3');
-      nav_item_5.removeClass('slide-in-nav-item-delay-4-reverse').addClass('slide-in-nav-item-delay-4');
-    } else {
-      top_bar.removeClass('animate-top-bar').addClass('animate-out-top-bar');
-      middle_bar.removeClass('animate-middle-bar').addClass('animate-out-middle-bar');
-      bottom_bar.removeClass('animate-bottom-bar').addClass('animate-out-bottom-bar');
-      overlay_navigation.removeClass('overlay-slide-down').addClass('overlay-slide-up')
-      nav_item_1.removeClass('slide-in-nav-item').addClass('slide-in-nav-item-reverse');
-      nav_item_2.removeClass('slide-in-nav-item-delay-1').addClass('slide-in-nav-item-delay-1-reverse');
-      nav_item_3.removeClass('slide-in-nav-item-delay-2').addClass('slide-in-nav-item-delay-2-reverse');
-      nav_item_4.removeClass('slide-in-nav-item-delay-3').addClass('slide-in-nav-item-delay-3-reverse');
-      nav_item_5.removeClass('slide-in-nav-item-delay-4').addClass('slide-in-nav-item-delay-4-reverse');
-    }
-  })
+  $('.open-overlay').click(function() {
+      var overlay_navigation = $('.overlay-navigation'),
+          top_bar = $('.bar-top'),
+          middle_bar = $('.bar-middle'),
+          bottom_bar = $('.bar-bottom');
+
+      overlay_navigation.toggleClass('overlay-active');
+      if (overlay_navigation.hasClass('overlay-active')) {
+          overlay_navigation.velocity('transition.slideDownIn', {duration: 300});
+          
+          // Animating each bar of the burger menu
+          top_bar.addClass('animate-top-bar');
+          middle_bar.addClass('animate-middle-bar');
+          bottom_bar.addClass('animate-bottom-bar');
+
+          // Animating list items with Velocity.js
+          $('nav ul li').velocity('transition.slideRightIn', {
+              stagger: 150,
+              complete: function() {
+                  $('nav ul li a').velocity({ opacity: [1, 0] }, { duration: 140 });
+              }
+          });
+      } else {
+          overlay_navigation.velocity('transition.slideUpOut', {duration: 300});
+          
+          // Reversing the animation of each bar
+          top_bar.removeClass('animate-top-bar').addClass('animate-out-top-bar');
+          middle_bar.removeClass('animate-middle-bar').addClass('animate-out-middle-bar');
+          bottom_bar.removeClass('animate-bottom-bar').addClass('animate-out-bottom-bar');
+
+          $('nav ul li').velocity('transition.slideLeftOut', {
+              stagger: 150,
+              complete: function() {
+                  $('nav ul li a').velocity({ opacity: [0, 1] }, { duration: 50 });
+              }
+          });
+      }
+  });
 
   document.addEventListener('DOMContentLoaded', () => {
     const navLinks = document.querySelectorAll('.overlay-navigation ul li a');
